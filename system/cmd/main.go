e89177660d1492f284a93bc43d55dd8abdb10ec6
alik <mamedovalik267@gmail.com>
2025-02-02 01:11:55 +0400

    second change

---
+++ system/cmd/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"UpperRestApi/chain"
+	"UpperRestApi/connect"
+	"UpperRestApi/repo"
+	"log"
+	"net/http"
+)
+
+func main() {
+	db := connect.Connect()                               // Подключение к базе данных
+	defer db.Close()                                      // Закрытие базы данных при завершении
+	repository := repo.NewRepo(db)                        // Создаем объект Repo
+	chain.AttachRouts(repository.R, repository.Router)    // Подключаем маршруты с помощью Repo
+	log.Fatal(http.ListenAndServe(":8087", repository.R)) // Запуск сервера с роутером из Repo
+}
